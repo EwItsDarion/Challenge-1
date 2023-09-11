726b9a2a317cd44701726eaab81a615c570234af
EwItsDarion <131904023+EwItsDarion@users.noreply.github.com>
2023-09-10 23:07:41 -0500

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿/*
+     * Darion Jeffries
+     * FollowPlayerX
+     * Challenge1
+     * Fixed camera that follows player
+     */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+
+    // Update is called once per frame
+
+    private Vector3 offset = new Vector3(20, 2, 7);
+   
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/TargetPassThrough.cs
@@ -0,0 +1,23 @@
+﻿/*
+     * Darion Jeffries
+     * Target Pass Through
+     * Challenge1
+     * When colliding with trigger, adds to score.
+     */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetPassThrough : MonoBehaviour
+{
+    public Text textbox;
+      
+    private void onTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScoreManager.score++;
+        }
+    }
+}
